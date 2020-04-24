f0ebff44e8cee8ab0e9ac601da43cd3e2d00d90f
V S Sreejeet <vsreejeet@gmail.com>
2020-04-25 01:13:09 +0530

    updated implement-strstr.go

--- implement-strstr.go
+++ implement-strstr.go
@@ -43,34 +43,3 @@ func main() {
 	fmt.Printf("Yields : %v\n", result)
 
 }
-
-/*
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-		if len(needle) == 0 {
-			return 0
-		}
-		if len(needle) > len(haystack) {
-			return -1
-		}
-		for i := 0; i < len(haystack); i++ {
-			if haystack[i] == needle[0] && len(haystack)-i >= len(needle) {
-				foundNeedle := true
-				for j := 0; j < len(needle) && j < len(haystack); j++ {
-					if needle[j] != haystack[i+j] {
-						foundNeedle = false
-						break
-					}
-				}
-				if foundNeedle {
-					return i
-				}
-			}
-		}
-		return -1
-	}
-    }
-};
-
-}*/
