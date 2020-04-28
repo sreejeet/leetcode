e2b40cd50eae5fd5ea1b59f2ca56a2cde6ce11b4
V S Sreejeet <vsreejeet@gmail.com>
2020-04-29 01:25:12 +0530

    added plus-one.go

---
+++ plus-one.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; ; i-- {
+		digits[i] = (digits[i] + 1) % 10
+		if digits[i] != 0 {
+			break
+		}
+		if i == 0 {
+			digits = append([]int{1}, digits...)
+			break
+		}
+	}
+	return digits
+}
+
+func main() {
+	test := []int{0, 0}
+	fmt.Printf("Testing: %v\n", test)
+	result := plusOne(test)
+	fmt.Printf("Yields : %v\n", result)
+}
