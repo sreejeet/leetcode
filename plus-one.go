5d8f7ff77e508670092cddf4f8640d6c19b866c5
V S Sreejeet <vsreejeet@gmail.com>
2020-04-29 01:27:29 +0530

    updated plus-one.go

--- plus-one.go
+++ plus-one.go
@@ -8,14 +8,13 @@ func plusOne(digits []int) []int {
 	for i := len(digits) - 1; ; i-- {
 		digits[i] = (digits[i] + 1) % 10
 		if digits[i] != 0 {
-			break
+			return digits
 		}
 		if i == 0 {
 			digits = append([]int{1}, digits...)
-			break
+			return digits
 		}
 	}
-	return digits
 }
 
 func main() {
