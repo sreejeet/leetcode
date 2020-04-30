6bc2e78ac5a9b8a7e2b19a35be87339fb1e8b6b0
V S Sreejeet <vsreejeet@gmail.com>
2020-04-30 09:29:39 +0530

    updated concurrency/print-foobar-alternately.py

--- concurrency/print-foobar-alternately.py
+++ concurrency/print-foobar-alternately.py
@@ -1,3 +1,32 @@
+""" Using threading.Event """
+from threading import Event
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_lock = Event()
+        self.bar_lock = Event()
+        self.foo_lock.set()
+
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.foo_lock.wait()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            self.foo_lock.clear()
+            self.bar_lock.set()
+
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.bar_lock.wait()
+            # printBar() outputs "bar". Do not change or remove this line.
+            printBar()
+            self.bar_lock.clear()
+            self.foo_lock.set()
+
+
+""" Using threading.Lock """
 from threading import Lock
 class FooBar:
     def __init__(self, n):
