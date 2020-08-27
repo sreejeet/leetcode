18f542c3e7c54a882f0ad578718d746a4ba4a36a
V S Sreejeet <vsreejeet@gmail.com>
2020-08-27 19:16:27 +0530

    add algorithms/letter-combinations-of-a-phone-number.py

---
+++ algorithms/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        keypad = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz",
+        }
+
+        ans_list = []
+        max_l = len(digits)
+
+        alphas = [keypad[int(x)] for x in digits]
+
+        def r(ans="", chars_i=0):
+            chars = alphas[chars_i] if chars_i < max_l else []
+            nex = alphas[chars_i+1] if chars_i+1 < max_l else []
+            if nex == []:
+                for c in chars:
+                    ans_list.append(ans+c)
+            else:
+                for c in chars:
+                    r(ans+c, chars_i+1)
+
+        r()
+        return ans_list
